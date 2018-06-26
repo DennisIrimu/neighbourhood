6a5c9e09cd747123c1209c07dc0b376ae8ef8b7c
dnyt <lyricaldnyt@gmail.com>
2018-06-26 19:01:28 +0300

    Create user profile view class

--- clan/neighbour/views.py
+++ clan/neighbour/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
-
+from .forms import UserProfileForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login,authenticate
 # Create your views here.
+
+@login_required(login_url='/accounts/login/')
+def profile(request,id):
+    '''
+    function to display the user profile
+    '''
+    current_user = request.user
+    current_user.id = request.user.id
+    current_profile = UserProfile.objects.get(user_id = current_user.id)
+    try:
+        profile = UserProfile.objects.get(user_id = current_user.id)
+
+
+        return render(request, 'profile.html', {"profile":profile,"id":current_user.id})
+
+
+    except ValueError:
+        raise Http404()
