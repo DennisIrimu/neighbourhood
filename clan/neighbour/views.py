fdb70eb0ff23d6a6341a9059ae7d68574571ae9a
dnyt <lyricaldnyt@gmail.com>
2018-06-27 15:16:34 +0300

    Create index template

--- clan/neighbour/views.py
+++ clan/neighbour/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-from .forms import UserProfileForm
+from .forms import UserProfileForm,NeighborhoodForm,BusinessForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login,authenticate
+from .models import UserProfile,Neighborhood,Business,Service
+from django.http import Http404
 # Create your views here.
 
 @login_required(login_url='/accounts/login/')
@@ -22,3 +24,7 @@ def profile(request,id):
 
     except ValueError:
         raise Http404()
+
+def index(request):
+    neighborhood = Neighborhood.objects.all()
+    return render(request,'index.html',{"neighborhood":Neighborhood})
